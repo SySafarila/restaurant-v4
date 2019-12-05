2f0164bac6ad6b4b989368b2ac46fcc141144776
Syahrul Safarila <sysafarila.official@gmail.com>
2019-12-05 12:37:39 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,51 +21,51 @@
                             Hi Owner
                         @else
                             @if (Auth::user()->level == 'Admin')
-                            <div class="col-md-4 col-6 mb-4">
-                                <div class="card ">
-                                    <div class="card-header">Header</div>
-                                    <div class="card-body">
-                                      <h5 class="card-title">Light card title</h5>
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <div class="col-md-4 col-6 mb-4">
+                                    <div class="card ">
+                                        <div class="card-header">Header</div>
+                                        <div class="card-body">
+                                        <h5 class="card-title">Light card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4 col-6 mb-4">
-                                <div class="card ">
-                                    <div class="card-header">Header</div>
-                                    <div class="card-body">
-                                      <h5 class="card-title">Light card title</h5>
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <div class="col-md-4 col-6 mb-4">
+                                    <div class="card ">
+                                        <div class="card-header">Header</div>
+                                        <div class="card-body">
+                                        <h5 class="card-title">Light card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4 col-6 mb-4">
-                                <div class="card ">
-                                    <div class="card-header">Header</div>
-                                    <div class="card-body">
-                                      <h5 class="card-title">Light card title</h5>
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <div class="col-md-4 col-6 mb-4">
+                                    <div class="card ">
+                                        <div class="card-header">Header</div>
+                                        <div class="card-body">
+                                        <h5 class="card-title">Light card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4 col-6 mb-4">
-                                <div class="card ">
-                                    <div class="card-header">Header</div>
-                                    <div class="card-body">
-                                      <h5 class="card-title">Light card title</h5>
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <div class="col-md-4 col-6 mb-4">
+                                    <div class="card ">
+                                        <div class="card-header">Header</div>
+                                        <div class="card-body">
+                                        <h5 class="card-title">Light card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div class="col-md-4 col-6 mb-4">
-                                <div class="card ">
-                                    <div class="card-header">Header</div>
-                                    <div class="card-body">
-                                      <h5 class="card-title">Light card title</h5>
-                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <div class="col-md-4 col-6 mb-4">
+                                    <div class="card ">
+                                        <div class="card-header">Header</div>
+                                        <div class="card-body">
+                                        <h5 class="card-title">Light card title</h5>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
                             @else
                                 @if (Auth::user()->level == 'Cashier')
                                     Hi Cashier
