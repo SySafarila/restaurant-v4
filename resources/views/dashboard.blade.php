1953eef030d292a218973685a079d04313990f17
Syahrul Safarila <sysafarila.official@gmail.com>
2019-12-05 12:37:04 +0700

    Update dashboard.blade.php
    - Add item for Admin

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+        {{-- <div class="col-md-8"> --}}
+            {{-- <div class="card"> --}}
+                {{-- <div class="card-header">Dashboard</div> --}}
 
-                <div class="card-body">
+                {{-- <div class="card-body"> --}}
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -21,7 +21,51 @@
                             Hi Owner
                         @else
                             @if (Auth::user()->level == 'Admin')
-                                Hi Admin
+                            <div class="col-md-4 col-6 mb-4">
+                                <div class="card ">
+                                    <div class="card-header">Header</div>
+                                    <div class="card-body">
+                                      <h5 class="card-title">Light card title</h5>
+                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-6 mb-4">
+                                <div class="card ">
+                                    <div class="card-header">Header</div>
+                                    <div class="card-body">
+                                      <h5 class="card-title">Light card title</h5>
+                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-6 mb-4">
+                                <div class="card ">
+                                    <div class="card-header">Header</div>
+                                    <div class="card-body">
+                                      <h5 class="card-title">Light card title</h5>
+                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-6 mb-4">
+                                <div class="card ">
+                                    <div class="card-header">Header</div>
+                                    <div class="card-body">
+                                      <h5 class="card-title">Light card title</h5>
+                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-6 mb-4">
+                                <div class="card ">
+                                    <div class="card-header">Header</div>
+                                    <div class="card-body">
+                                      <h5 class="card-title">Light card title</h5>
+                                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                    </div>
+                                </div>
+                            </div>
                             @else
                                 @if (Auth::user()->level == 'Cashier')
                                     Hi Cashier
@@ -41,9 +85,9 @@
                     @else
                         Hi <b>{{ Auth::user()->name }}</b>, You're nonactive as <b>{{ Auth::user()->level }}</b>
                     @endif
-                </div>
-            </div>
-        </div>
+                {{-- </div> --}}
+            {{-- </div> --}}
+        {{-- </div> --}}
     </div>
 </div>
 @endsection
