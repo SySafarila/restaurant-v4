89468d853b692219a406cf2b3b4914fe2f4cfb32
Syahrul Safarila <sysafarila.official@gmail.com>
2019-12-06 17:40:15 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,54 +21,36 @@
                             Hi Owner
                         @else
                             @if (Auth::user()->level == 'Admin')
-                                <div class="col-md-4 col-12 mb-1">
-                                    <div class="card mb-3">
-                                        <div class="row no-gutters">
-                                            <div class="col-4">
-                                                <img src="http://www.attachmax.com/p/2018/12/restaurant-menu-design-free-psd-free-photoshop-downloads-pertaining-to-menu-cover-design-templates-600x600.jpg" class="card-img h-100" alt="...">
-                                            </div>
-                                            <div class="col-8">
-                                            <div class="card-body">
-                                                <h5 class="card-title">Menus</h5>
-                                                <p class="card-text">Add, edit, or delete menus.</p>
-                                                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
-                                            </div>
-                                            </div>
-                                        </div>
+                                <div class="col-md-4 col-12 mb-4">
+                                    <div class="card">
+                                        <a href="#" class="card-body text-decoration-none">
+                                            <h5 class="card-title text-dark">Menus Panel</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted">Show, add, edit, or delete</h6>
+                                        </a>
                                     </div>
                                 </div>
-
-                                <div class="col-md-4 col-12 mb-1">
-                                    <div class="card mb-3">
-                                        <div class="row no-gutters">
-                                            <div class="col-4">
-                                                <img src="http://www.attachmax.com/p/2018/12/restaurant-menu-design-free-psd-free-photoshop-downloads-pertaining-to-menu-cover-design-templates-600x600.jpg" class="card-img h-100" alt="...">
-                                            </div>
-                                            <div class="col-8">
-                                            <div class="card-body">
-                                                <h5 class="card-title">Users</h5>
-                                                <p class="card-text">Show, edit, or delete users.</p>
-                                                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
-                                            </div>
-                                            </div>
-                                        </div>
+                                <div class="col-md-4 col-12 mb-4">
+                                    <div class="card">
+                                        <a href="#" class="card-body text-decoration-none">
+                                            <h5 class="card-title text-dark">Menus Panel</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted">Show, add, edit, or delete</h6>
+                                        </a>
                                     </div>
                                 </div>
-
-                                <div class="col-md-4 col-12 mb-1">
-                                    <div class="card mb-3">
-                                        <div class="row no-gutters">
-                                            <div class="col-4">
-                                                <img src="http://www.attachmax.com/p/2018/12/restaurant-menu-design-free-psd-free-photoshop-downloads-pertaining-to-menu-cover-design-templates-600x600.jpg" class="card-img h-100" alt="...">
-                                            </div>
-                                            <div class="col-8">
-                                            <div class="card-body">
-                                                <h5 class="card-title">Employees</h5>
-                                                <p class="card-text">Show, edit, or delete Employeessssssssssssssssssssssssssssssssssssssssssssssssssssssssss.</p>
-                                                {{-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> --}}
-                                            </div>
-                                            </div>
-                                        </div>
+                                <div class="col-md-4 col-12 mb-4">
+                                    <div class="card">
+                                        <a href="#" class="card-body text-decoration-none">
+                                            <h5 class="card-title text-dark">Menus Panel</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted">Show, add, edit, or delete</h6>
+                                        </a>
+                                    </div>
+                                </div>
+                                <div class="col-md-4 col-12 mb-4">
+                                    <div class="card">
+                                        <a href="#" class="card-body text-decoration-none">
+                                            <h5 class="card-title text-dark">Menus Panel</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted">Show, add, edit, or delete</h6>
+                                        </a>
                                     </div>
                                 </div>
                             @else
