629ca760202cf4681380fb638521cf1965990c3d
Syahrul Safarila <sysafarila.official@gmail.com>
2019-12-09 01:13:21 +0700

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -32,6 +32,14 @@
                         {{-- OWNER PAGE --}}
                             <div class="col-md-4 col-12 mb-4">
                                 <div class="card">
+                                    <a href="{{ route('users.index') }}" class="card-body text-decoration-none">
+                                        <h5 class="card-title text-dark">Users Panel</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Show, add, edit, or delete</h6>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-12 mb-4">
+                                <div class="card">
                                     <a href="#" class="card-body text-decoration-none">
                                         <h5 class="card-title text-dark">Employees Panel</h5>
                                         <h6 class="card-subtitle mb-2 text-muted">Show, add, edit, or delete</h6>
