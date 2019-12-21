c9ae0cb7aff50f7af3eac2aefc0dc331f103be9b
Syahrul Safarila <sysafarila.official@gmail.com>
2019-12-21 14:48:55 +0700

    Update responsive table for Orders

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -7,43 +7,43 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-6 col-sm-12">
+            <div class="col-md-9 col-sm-12">
                 <div class="card">
-                    <div class="card-body table-responsive">
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <div class="row">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered">
+                                <thead>
+                                    <tr>
                                         <th class="text-center">No</th>
                                         <th>Name</th>
                                         <th class="text-center">Quantity</th>
                                         <th class="text-center">Price</th>
                                         <th class="text-center">Total</th>
-                                    </div>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($orders as $order)
-                                    <tr>
-                                        <td class="text-center align-middle">{{ $number++ }}</td>
-                                        <td class="align-middle">{{ $order->menu->name }}</td>
-                                        <td class="text-center align-middle">{{ $order->quantity }}</td>
-                                        <td class="text-center align-middle">{{ number_format($order->menu->price) }}</td>
-                                        <td class="text-center align-middle">{{ number_format($order->menu->price * $order->quantity) }}</td>
                                     </tr>
-                                @endforeach
-                                @if ($orders->sum('total') < 1)
-                                <tr>
-                                    <td colspan="5" class="text-center">Empty</td>
-                                </tr>
-                                @else
-                                <tr>
-                                    <td colspan="4" class="text-center align-middle">Total ( Rp )</td>
-                                    <td class="text-center align-middle">{{ number_format($orders->sum('total')) }}</td>
-                                </tr>
-                                @endif
-                            </tbody>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    @foreach ($orders as $order)
+                                        <tr>
+                                            <td class="text-center align-middle">{{ $number++ }}</td>
+                                            <td class="align-middle">{{ $order->menu->name }}</td>
+                                            <td class="text-center align-middle">{{ $order->quantity }}</td>
+                                            <td class="text-center align-middle">{{ number_format($order->menu->price) }}</td>
+                                            <td class="text-center align-middle">{{ number_format($order->menu->price * $order->quantity) }}</td>
+                                        </tr>
+                                    @endforeach
+                                    @if ($orders->sum('total') < 1)
+                                    <tr>
+                                        <td colspan="5" class="text-center">Empty</td>
+                                    </tr>
+                                    @else
+                                    <tr>
+                                        <td colspan="4" class="text-center align-middle">Total ( Rp )</td>
+                                        <td class="text-center align-middle">{{ number_format($orders->sum('total')) }}</td>
+                                    </tr>
+                                    @endif
+                                </tbody>
+                            </table>
+                        </div>
                         @if (count($orders) < 1)
                         <div class="d-flex justify-content-center">
                             <a href="{{ route('menus.index') }}" class="btn btn-sm btn-success">Go to Menus</a>
