85528bfd2987f89917617dc496501cfaaf2f75be
Syahrul Safarila <sysafarila.official@gmail.com>
2019-12-23 18:45:55 +0700

    Update css and add custom font

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm sticky-top">
             <div class="container-fluid">
-                <a class="navbar-brand text-success" href="{{ url('/') }}">
-                    Restaurant V4
+                <a class="navbar-brand text-success my-font" href="{{ url('/') }}">
+                    Restaurant v4
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
