4ee5ac09523770aa36b0804acd97415613d54f45
Syahrul Safarila <sysafarila.official@gmail.com>
2019-12-26 12:20:25 +0700

    Update seeder

--- database/seeds/MenusSeeder.php
+++ database/seeds/MenusSeeder.php
@@ -12,18 +12,22 @@ class MenusSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker::create();
-        foreach (range(1,20) as $index)
-        {
 	        DB::table('menus')->insert([
 	            'name' => 'Nasi Goreng',
-                'description' => 'lorem ipsum dolor sit amet bla bla bla bla bla blablabla bla',
-                'price' => 10000,
-                'img' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSTX80vgg6_6gwliroivmk4Vtqx3p11E1XC4Qb-hVyg6YIag-DD',
+                'description' => 'Nasi goreng adalah sebuah makanan berupa nasi yang digoreng dan diaduk dalam minyak goreng atau margarin, biasanya ditambah kecap manis, bawang merah, bawang putih, asam jawa, lada dan bumbu-bumbu lainnya, seperti telur, ayam, dan kerupuk.',
+                'price' => 12000,
+                'img' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRLvXYXsRCdp_dzzArbON6jl94LXnFw5qa1CWBHw9YPiqvqNwIl',
+                'stock' => 99,
+                'status' => 'Available',
+            ]);
+            DB::table('menus')->insert([
+	            'name' => 'Ayam Goreng',
+                'description' => 'Ayam goreng Nusantara adalah hidangan Asia Tenggara yang merupakan ayam yang digoreng dalam minyak goreng. Dalam dunia internasional, istilah ayam goreng merujuk kepada ayam goreng gaya Nusantara.',
+                'price' => 7000,
+                'img' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRlvKNW2zD499zD07wjjbswL4CTes8BR9ChlECDTzgQXoEsz3nR',
                 'stock' => 99,
                 'status' => 'Available',
             ]);
             //php artisan db:seed --class=MenusSeeder
-            }
     }
 }
