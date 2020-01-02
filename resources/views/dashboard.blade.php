e1e2ce8288c17495078db4a223f5a9d426489d52
Syahrul Safarila <sysafarila.official@gmail.com>
2020-01-02 20:26:23 +0700

    Update System

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -139,8 +139,14 @@
                                 <div class="col-md-4 col-12 mb-3">
                                     <div class="card shadow-sm h-100">
                                         <a href="{{ route('invoices.index') }}" class="card-body text-decoration-none">
-                                            <h5 class="card-title text-dark">Transactions List</h5>
-                                            <h6 class="card-subtitle mb-2 text-muted">Invoices</h6>
+                                            <h5 class="card-title text-dark">Invoices</h5>
+                                            <h6 class="card-subtitle mb-2 text-muted">
+                                                @if (Auth::user()->invoices->count() == 0)
+                                                    You don't have any invoices
+                                                @else
+                                                    You have <u>{{ Auth::user()->invoices->count() }}</u> invoices
+                                                @endif
+                                            </h6>
                                         </a>
                                     </div>
                                 </div>
