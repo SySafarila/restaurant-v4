8cf9e07bbb3a413612c14b25d85274d5f99db152
Syahrul Safarila <sysafarila.official@gmail.com>
2020-01-02 14:56:36 +0700

    Update System

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -85,7 +85,7 @@
                                     @endif
                                 </tbody>
                             </table>
-                            <p class="text-center text-muted">If some of Order is disappeared, it's mean the Menus is Deleted by Admin</p>
+                            <p class="text-center text-muted">If several Orders disappear, it means the Order has been deleted by the Admin</p>
                         </div>
                         @if (count($orders) < 1)
                         <div class="d-flex justify-content-center">
