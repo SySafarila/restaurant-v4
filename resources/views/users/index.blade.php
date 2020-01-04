4a0777159efb27661e75d7e18f26f0a38dfeaf3e
Syahrul Safarila <sysafarila.official@gmail.com>
2020-01-04 09:54:47 +0700

    Update System

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,43 +12,45 @@
                 </div>
             @endif
             <div class="card">
-                <div class="card-body table-responsive">
+                <div class="card-body">
                     <form action="{{ route('users.search') }}" method="get" class="mb-3">
                         <div class="d-flex">
                             <input type="text" name="username" class="rounded-pill form-control form-control-sm mr-1 @error('username') is-invalid @enderror" placeholder="@error('username') {{ $message }} @else Search username @enderror" value="{{ old('username') }}" required>
                             <button type="submit" class="rounded-pill btn btn-sm btn-success"><span class="material-icons align-middle" style="font-size:15px; padding-bottom:3px;">search</span></button>
                         </div>
                     </form>
-                    <table class="table table-bordered table-hover">
-                        <thead>
-                            <tr>
-                                <th scope="col" class="text-center">No</th>
-                                <th scope="col" class="w-75">Name</th>
-                                <th scope="col" class="text-center">Level</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($users as $user)
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-hover">
+                            <thead>
                                 <tr>
-                                    <td class="text-center">{{ $number++ }}</td>
-                                    <td><a href="user/{{ $user->id }}" class="text-decoration-none text-dark">{{ $user->name }}</a></td>
-                                    <td class="text-center">
-                                        <span class="badge 
-                                            @if($user->level == 'Admin')
-                                                badge-success 
-                                            @else
-                                                @if($user->level == 'Owner')
-                                                    badge-warning
-                                                @else
-                                                    badge-secondary
-                                                @endif 
-                                            @endif">{{ $user->level }}
-                                        </span>
-                                    </td>
+                                    <th scope="col" class="text-center">No</th>
+                                    <th scope="col" class="w-75">Name</th>
+                                    <th scope="col" class="text-center">Level</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td class="text-center">{{ $number++ }}</td>
+                                        <td><a href="user/{{ $user->id }}" class="text-decoration-none text-dark">{{ $user->name }}</a></td>
+                                        <td class="text-center">
+                                            <span class="badge 
+                                                @if($user->level == 'Admin')
+                                                    badge-success 
+                                                @else
+                                                    @if($user->level == 'Owner')
+                                                        badge-warning
+                                                    @else
+                                                        badge-secondary
+                                                    @endif 
+                                                @endif">{{ $user->level }}
+                                            </span>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
