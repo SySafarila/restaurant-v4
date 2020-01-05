9ae822d05d80e751413a74ad79a8c79f6aa0cd26
Syahrul Safarila <sysafarila.official@gmail.com>
2020-01-05 18:21:35 +0700

    Update System

--- resources/views/users/search.blade.php
+++ resources/views/users/search.blade.php
@@ -6,15 +6,19 @@
 
 @section('content')
     <div class="container">
-        <div class="card">
-            <div class="card-body">
-                @empty($user)
-                    <h1 class="text-muted text-center">Empty</h1>
-                @else
-                    <p>{{ $user->username }}</p>
-                    <p>{{ $user->name }}</p>
-                    <p>{{ $user->email }}</p>
-                @endempty
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <div class="card shadow-sm">
+                    <div class="card-body">
+                        @empty($user)
+                            <h1 class="text-muted text-center">Empty</h1>
+                        @else
+                            <p>{{ $user->username }}</p>
+                            <p>{{ $user->name }}</p>
+                            <p>{{ $user->email }}</p>
+                        @endempty
+                    </div>
+                </div>
             </div>
         </div>
     </div>
