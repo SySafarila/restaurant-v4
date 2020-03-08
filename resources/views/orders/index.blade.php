6e7f876f0b8f8928c031a5890d66ab4105db5265
Syahrul Safarila <sysafarila.official@gmail.com>
2020-03-08 08:26:55 +0700

    Update index.blade.php

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -31,78 +31,34 @@
             </div>
         @endif
         <div class="row justify-content-center">
-            <div class="col-md-9 col-sm-12">
-                {{-- <div class="card shadow-sm"> --}}
-                    {{-- <div class="card-body"> --}}
-                        <div class="table-responsive">
-                            <table class="table table-bordered shadow-sm">
-                                <thead>
-                                    <tr>
-                                        <th colspan="5" class="text-center">Pending</th>
-                                    </tr>
-                                    <tr>
-                                        {{-- <div class="row"> --}}
-                                            <th class="text-center" scope="col">No</th>
-                                            <th class="text-left" scope="col">Name</th>
-                                            <th class="text-center" scope="col">Quantity</th>
-                                            <th class="text-center" scope="col">Price</th>
-                                            <th class="text-center" scope="col">Total</th>
-                                        {{-- </div> --}}
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($orders as $order)
-                                        <tr>
-                                            <td class="text-center align-middle">{{ $number++ }}</td>
-                                            <td class="align-middle text-nowrap">
-                                                {{ $order->menu->name }} <span class="badge @if($order->status == 'Pending') badge-warning @else badge-primary @endif">@if($order->status == 'Pending') Pending @else Cooking @endif</span>
-                                                <br>
-                                                @if ($order->status == 'Pending')
-                                                <a href="{{ route('orders.edit', $order->id) }}" class="badge badge-success">Edit</a> <a class="text-decoration-none badge badge-danger align-middle" href="{{ route('orders.destroyOne', $order->id) }}" onclick="event.preventDefault(); document.getElementById('orders.destroyOne').submit();">{{ __('Delete') }}</a>
-                                                @endif
-                                                <form id="orders.destroyOne" action="{{ route('orders.destroyOne', $order->id) }}" method="post" class="d-none">
-                                                    @csrf
-                                                    @method('delete')
-                                                    <div class="d-flex justify-content-center">
-                                                        <button class="btn btn-sm btn-outline-danger" type="submit">destroyOne</button>
-                                                    </div>
-                                                </form>
-                                            </td>
-                                            <td class="text-center align-middle">{{ $order->quantity }}</td>
-                                            <td class="text-center align-middle">{{ number_format($order->menu->price, 0, 0, '.') }}</td>
-                                            <td class="text-center align-middle">{{ number_format($order->menu->price * $order->quantity, 0, 0, '.') }}</td>
-                                        </tr>
-                                    @endforeach
-                                    @if ($orders->sum('total') < 1)
-                                    <tr>
-                                        <td colspan="5" class="text-center">Empty</td>
-                                    </tr>
-                                    @else
-                                    <tr>
-                                        <td colspan="4" class="text-center align-middle font-weight-bold text-success">Total ( Rp )</td>
-                                        <td class="text-center align-middle text-orange font-weight-bold">{{ number_format($orders->sum('total'), 0, 0, '.') }}</td>
-                                    </tr>
-                                    @endif
-                                </tbody>
-                            </table>
+            @foreach ($orders as $order)
+            <div class="col-md-6 mb-3">
+                <div class="card shadow-sm h-100">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-4">
+                                <img src="{{ $order->menu->img }}" class="card-img shadow-sm" alt="">
+                            </div>
+                            <div class="col">
+                                <p class="card-title font-weight-bold m-0">{{ $order->menu->name }}</p>
+                                <span class="text-success">{{ $order->quantity }}</span> <span class="font-weight-bold">x</span> <span class="text-orange font-weight-bold">{{ number_format($order->menu->price, 0, 0, '.') }}</span>
+                                    <br>
+                                @if ($order->menu->stock == 0)
+                                    <span class="badge badge-warning algn-middle">Out of stock</span>
+                                @else
+                                    <span class="badge badge-success align-middle"><a href="{{ route('orders.edit', $order->id) }}" class="text-white text-decoration-none">Edit</a></span>
+                                @endif
+                                <a href="{{ route('orders.destroyOne', $order->id) }}" class="badge badge-danger align-middle" onclick="event.preventDefault();document.getElementById('destroyOne').submit();">Delete</a>
+                                <form id="destroyOne" action="{{ route('orders.destroyOne', $order->id) }}" method="POST" style="display: none;">
+                                    @csrf
+                                    @method('DELETE')
+                                </form>
+                            </div>
                         </div>
-                        <p class="text-center text-muted">If several Orders disappear, it means the Orders has been deleted by the Admin</p>
-                        @if (count($orders) < 1)
-                        <div class="d-flex justify-content-center">
-                            <a href="{{ route('menus.index') }}" class="btn btn-sm btn-success mb-3">Order Now</a>
-                        {{-- </div> --}}
-                        @else
-                            <form action="{{ route('orders.destroy') }}" method="post">
-                                @csrf
-                                @method('delete')
-                                <div class="d-flex justify-content-center">
-                                    <button class="btn btn-sm btn-outline-danger mb-3" type="submit">Empty Orders</button>
-                                </div>
-                            </form>
-                        @endif
-                    {{-- </div> --}}
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
 @endsection
