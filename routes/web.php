04621b45624d5ec68550cc0c1a5587fe75963568
Syahrul Safarila <sysafarila.official@gmail.com>
2020-03-16 18:30:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -38,8 +38,8 @@ Route::patch('/dashboard/user/{id}', 'UsersController@update')->name('users.upda
 Route::delete('/dashboard/users/{id}', 'UsersController@destroy')->name('users.delete');
 // Search
 Route::get('/dashboard/search', 'UsersController@search')->name('users.search');
-Route::get('/dashboard/user/search/username/{username}', 'UsersController@search2')->name('users.search2');
-Route::get('/dashboard/{user:username}/payment', 'UsersController@payment')->name('users.payment');
+// Route::get('/dashboard/user/search/username/{username}', 'UsersController@search2')->name('users.search2');
+Route::get('/dashboard/{user:username}/payment', 'UsersController@payment')->middleware('admin')->name('users.payment');
 
 // Menus
 Route::get('/dashboard/menus', 'MenusController@index')->name('menus.index');
