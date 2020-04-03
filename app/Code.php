c1c7984ebaa2f3a7db20644bcb6c826cb127161c
Syahrul Safarila <sysafarila.official@gmail.com>
2020-04-03 15:05:59 +0700

    Update System Database

---
+++ app/Code.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Code extends Model
+{
+    protected $fillable = ['unique_code', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function invoices()
+    {
+        return $this->hasMany('App\Invoice');
+    }
+}
