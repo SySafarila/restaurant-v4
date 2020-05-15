3852b17004ce1e6ae41ba20ce193d883792a8c47
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2020-05-15 21:23:24 +0700

    Create sidebar component (#30)

---
+++ resources/views/components/sidebar/sidebar-toggle.blade.php
@@ -0,0 +1,3 @@
+<div class="sidebar-button" id="sidebar-button">
+    <button type="button" class="btn material-icons sidebar-button-toggle" style="box-shadow: none;" onclick="sidebar()">more_vert</button>
+</div>
---
+++ resources/views/components/sidebar/sidebar.blade.php
@@ -0,0 +1,10 @@
+<main class="sidebar bg-white shadow-sm" id="sidebar">
+    <ul style="list-style-type: none; padding-left: 0px; padding-bottom: 2.5rem;">
+        <a href="{{ route('setting.overview') }}" class="text-decoration-none text-dark">
+            <li class="list-hover py-2 sidebar-padding-left {{ Request::is(['settings']) ? 'sidebar-active' : '' }}">Overview</li>
+        </a>
+        <a href="{{ route('setting.account') }}" class="text-decoration-none text-dark">
+            <li class="list-hover py-2 sidebar-padding-left {{ Request::is(['setting/account']) ? 'sidebar-active' : '' }}">Account</li>
+        </a>
+    </ul>
+</main>
