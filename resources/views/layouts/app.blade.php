e66a7064e2a01f4df898cffc851fc8d19be79c8d
Syahrul Safarila <57931052+SySafarila@users.noreply.github.com>
2020-05-17 13:14:46 +0700

    Dev setting (#34)
    * Update z-index of sidebar
    
    * Update z-index of sidebar toggle
    
    * Add shadow for each card on overview
    
    * Update z-index of Sidebar and the toggle
    
    * Update UI
    
    * Update UI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,9 +61,7 @@
                         <li class="nav-item">
                             <form action="{{ route('menus.search') }}" method="get">
                                 <div class="d-flex">
-                                    <input type="text" name="name" class="rounded-pill form-control form-control-sm d-none d-md-block search-top text-truncate" placeholder="Did You Looking for Food's or Drink's ?">
-                                    {{-- <input type="text" name="name" class="rounded-pill form-control form-control-sm d-none d-lg-block d-xl-none search-top" placeholder="Looking for Food's or Drink's ?">
-                                    <input type="text" name="name" class="rounded-pill form-control form-control-sm d-none d-xl-block search-top" placeholder="Looking for Food's or Drink's ?"> --}}
+                                    <input type="text" name="name" class="rounded-pill form-control form-control-sm d-none d-md-block search-top text-truncate" placeholder="Did You Looking for Food's or Drink's ?" required>
                                     <button type="submit" class="rounded-pill btn btn-sm btn-outline-success ml-1"><i class="material-icons align-middle" style="font-size:15px; padding-bottom:2px;">search</i></button>
                                 </div>
                             </form>
