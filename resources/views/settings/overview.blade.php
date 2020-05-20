5b67e9915b6f2c1ec6bd8d515cc7c7a2e3967597
Syahrul Safarila <sysafarila.official@gmail.com>
2020-05-20 14:29:01 +0700

    Add dropdown to Button on card

--- resources/views/settings/overview.blade.php
+++ resources/views/settings/overview.blade.php
@@ -9,7 +9,13 @@
         <div class="row px-2">
             <div class="col-md-6 mb-3 px-2">
                 <div class="card h-100 shadow-sm">
-                    <a href="{{ route('setting.account') }}" class="stretched-link"></a>
+                    {{-- <a href="{{ route('setting.account') }}" class="stretched-link"></a> --}}
+                    <div class="dropdown">
+                        <a href="#" class="material-icons position-absolute text-decoration-none more-vert" id="dropdownProfile" data-toggle="dropdown" style="right: 0.5rem; top: 0.5rem;">more_vert</a>
+                        <div class="dropdown-menu" aria-labelledby="dropdownProfile">
+                            <a class="dropdown-item" href="{{ route('setting.account') }}">Edit</a>
+                        </div>
+                    </div>
                     <div class="card-body d-flex">
                         <div class="col-2 h-100 p-0 d-flex align-items-center">
                             @if ($user->img == null)
@@ -39,6 +45,7 @@
             </div>
             <div class="col-md-6 mb-3 px-2">
                 <div class="card h-100 shadow-sm">
+                    <a href="#" class="material-icons position-absolute text-decoration-none more-vert" style="right: 0.5rem; top: 0.5rem;">more_vert</a>
                     <div class="card-body">
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis, nam amet voluptates provident iusto aperiam nemo eaque quae dolor accusantium magnam corporis quisquam, quia incidunt? Voluptatem officia ducimus quibusdam sunt!
                     </div>
@@ -46,6 +53,7 @@
             </div>
             <div class="col-md-6 mb-3 px-2">
                 <div class="card h-100 shadow-sm">
+                    <a href="#" class="material-icons position-absolute text-decoration-none more-vert" style="right: 0.5rem; top: 0.5rem;">more_vert</a>
                     <div class="card-body">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi minima amet dolorem ipsam eligendi fugiat delectus ratione expedita culpa ut assumenda, dolor illum nihil cumque, sequi eaque hic! Deserunt, veniam?
                     </div>
@@ -53,6 +61,7 @@
             </div>
             <div class="col-md-6 mb-3 px-2">
                 <div class="card h-100 shadow-sm">
+                    <a href="#" class="material-icons position-absolute text-decoration-none more-vert" style="right: 0.5rem; top: 0.5rem;">more_vert</a>
                     <div class="card-body">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum officiis tenetur perspiciatis exercitationem inventore veritatis deserunt odio vero eum omnis quos, assumenda beatae, minima aperiam sapiente maxime cum illum suscipit?
                     </div>
