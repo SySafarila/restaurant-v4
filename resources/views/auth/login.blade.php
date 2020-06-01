847f653579de7900511b1a993469b737b92e0f4b
Syahrul Safarila <sysafarila.official@gmail.com>
2020-06-01 19:38:25 +0700

    Update login UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,82 +1,41 @@
-@extends('layouts.app')
+@extends('layouts.login')
 
 @section('title', '| Login')
 
 @section('content')
 <div class="container">
+    <x-alert />
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success alert-dismissible fade show" role="alert">
-                            {{ session('status') }}
-                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                    @endif
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+        <div class="col-md-5">
+            <h1 class="material-icons d-block text-center" style="font-size: 5rem; margin-block: 2rem;">face</h1>
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text bg-transparent border-0 material-icons pl-0" id="email">email</span>
+                    </div>
+                    <input type="email" class="form-control bg-transparent border-0 px-0" placeholder="Email" name="email" value="{{ old('email') }}" style="box-shadow: none;" required autofocus>
                 </div>
-            </div>
+                <hr style="margin-top: -0.6rem;">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text bg-transparent border-0 material-icons pl-0" id="password">lock</span>
+                    </div>
+                    <input type="password" class="form-control bg-transparent border-0 px-0" placeholder="Password" name="password" style="box-shadow: none;" required>
+                </div>
+                <hr style="margin-top: -0.6rem;">
+                <div class="custom-control custom-checkbox mb-3">
+                    <input type="checkbox" class="custom-control-input" id="remember_me" name="remember">
+                    <label class="custom-control-label" for="remember_me">Remember Me</label>
+                </div>
+                <button type="submit" class="btn btn-block btn-success">Login</button>
+            </form>
+            @foreach ($errors->all() as $error)
+                <div class="d-flex mt-3">
+                    <p class="material-icons text-danger">info</p>
+                    <p class="text-danger" style="margin-left: 0.5rem;">{{ $error }}</p>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
---
+++ resources/views/layouts/login.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>Restaurant @yield('title')</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mystyle.css') }}" rel="stylesheet"> --}}
+    <link href="{{ asset('css/restaurantv4.css') }}" rel="stylesheet">
+</head>
+<body class="d-flex justify-content-center align-items-center" style="height: 80vh">
+    @yield('content')
+</body>
+<!-- Build With <3 -->
+</html>
