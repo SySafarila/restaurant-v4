b88b1761085181a093dc52d2ef921bf345e9387d
Syahrul Safarila <sysafarila.official@gmail.com>
2020-06-04 22:52:05 +0700

    Update validation

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -44,6 +44,13 @@
 @endsection
 @section('script')
 <script>
+    const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const email = document.getElementById('email');
+    if (regex.test(email.value) === true) {
+        email.classList.add('true');
+        email.style.borderColor = '#38c172';
+    }
+
     function validatingEmail() {
         const regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         const email = document.getElementById('email');
